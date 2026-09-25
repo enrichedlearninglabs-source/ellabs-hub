3c2464f19745cafbd3b4d5072fac6a188d2d5624
enrichedlearninglabs-source <enrichedlearninglabs@gmail.com>
2026-09-25 08:30:05 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -9,10 +9,11 @@
  * rules in firestore.rules.
  */
 window.ELLABS_FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyDkgQanaB3bfvzzfYKxx1fScrVc2e2UnSk",
+  authDomain: "ellabs-hub.firebaseapp.com",
+  projectId: "ellabs-hub",
+  storageBucket: "ellabs-hub.firebasestorage.app",
+  messagingSenderId: "311783329655",
+  appId: "1:311783329655:web:3369eb5b6a7f34bbd39b72",
+  measurementId: "G-BHRNJKRNNQ"
 };
